8834d59e0e5e9f7ccaa007ac3e52f259ad9e54a5
tony-vargas <jorvargas@outlook.com>
2023-11-12 18:50:31 -0800

    Linked js file to html

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Prompt the user for a list of froyo flavors separated by commas.
+const userInputString = prompt(
+    "Please list some froyo flavors separated by commas.",
+    "vanilla,strawberry,coffee"
+  );
+
+  // Split the string of numbers into an array of strings.
+const stringArray = userInputString.split(",");
+
 //object
 const froyo = {
     flavor: ["vanilla", "strawberry", "coffee"], 
